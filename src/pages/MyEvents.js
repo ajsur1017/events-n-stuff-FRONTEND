895d19db6a9b65d43a8daad2c24f1b5b6eb3c08c
Tom Ames <tsames1@gmail.com>
2021-11-23 17:24:37 -0800

    added MyEvents.js in Pages

---
+++ src/pages/MyEvents.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
+function MyEvent(props) {
+
+  const loaded = () => {
+    return ()
+
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+  
+  return (
+
+  )
+}
+
+export default MyEvent;
