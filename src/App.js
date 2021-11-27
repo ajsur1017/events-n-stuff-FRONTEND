c065ddb75231ebff920e5a5eb9b5866ef6499ccf
Anthony <ajsur1017@gmail.com>
2021-11-27 13:02:37 -0500

    updated home route

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <Header />
         <main>
           <Switch>
-            <Route exact path="/" render={(rp => gState.token? <h1>Dashboard</h1> :  <h1>Home</h1>)} />
+            <Route exact path="/" render={(rp => gState.token? <h1>Welcome!</h1> :  <h1>Signup or Login to get started!</h1>)} />
             <Route path="/signup" render={(rp => <Signup {...rp}/>)} />
             <Route path="/login" render={(rp => <Login {...rp}/>)} />
             {/* <Route path="/dashboard" render={(rp => <h1>Dashboard</h1>)} /> */}
