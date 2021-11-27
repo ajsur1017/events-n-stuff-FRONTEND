2a69298cc0d4996c4d49fa3b570c8973b0821e8d
Tom Ames <tsames1@gmail.com>
2021-11-26 16:00:23 -0800

    Added Bootstrap v5 to Index.html and re-styled navbar - still want to do some mouseover events for the icons in the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,31 +5,47 @@ import { GlobalCtx } from "../App"
 function Header(props) {
   const { gState, setGState } = React.useContext(GlobalCtx)
 
-  const logout = (
-    <Link>
-      <div className="logout" onClick={() => {
-        window.localStorage.removeItem("token")
-        setGState({ ...gState, token: null })
-      }}>Logout</div>
-    </Link>)
 
-  const login = (
+  //Conditional Left Side of NavBar
+  const navMe = (
     <>
-      <Link to="/login">
-        <div className="login">Login</div>
-      </Link>
-      <Link to="/signup">
-        <div className="signup">Sign Up</div>
-      </Link>
-    </>)
+      <li className={"nav-item"}><Link to="" className={"nav-link"}><i className={"bi-pen"}></i></Link></li>
+      <li className={"nav-item"}><Link to="" className={"nav-link"}><i className={"bi-people-fill"}></i></Link></li>
+    </>
+  )
+
+  //Logout Callback
+  const logout = () => {
+    window.localStorage.removeItem("token")
+    setGState({ ...gState, token: null })
+  }
+
+  //Conditional Right Side of NavBar
+  const userLogout = (
+    <>
+      <li className={"nav-item"}><Link className={"nav-link"}><i className={"bi-person-check-fill"}></i></Link></li>
+      <li onClick={logout} className={"nav-item"}><Link className={"nav-link"}><i className={"bi-x-circle"}></i></Link></li>
+    </>
+  )
+
+  const userLogin = (
+    <>
+      <li className={"nav-item"}><Link to="/signup" className={"nav-link"}><i className={"bi-plus-circle"}></i></Link></li>
+      <li className={"nav-item"}><Link to="/login" className={"nav-link"}><i className={"bi-arrow-up-right-circle"}></i></Link ></li>
+    </>
+  )
 
   return (
-    <nav className="nav">
-      <Link to="/">
-        <div className="title">Events n' Stuff</div>
-      </Link>
-      <div className="headerLinks">
-        {gState.token ? logout : login}
+    <nav className={"navbar navbar-expand-sm bg-dark navbar-dark fixed-top"}>
+      <div className={"container-fluid"}>
+        {/* <div className={"navbar-header"}> */}
+        <ul className={"navbar-nav"}>
+          <li className={"nav-item"}><Link to="/" className={"nav-link active"}><i className={"bi-house-fill"}></i></Link></li>
+          {gState.token ? navMe: null}
+        </ul>
+        <ul className="navbar-nav navbar-right">
+          {gState.token ? userLogout : userLogin}
+        </ul>
       </div>
     </nav>
   );
